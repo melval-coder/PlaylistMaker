b5d89f6cf8b80d1e476442bd5d87138bf51d2aaf
Valerik <val2567@yandex.ru>
2024-04-22 14:07:18 +0500

    10 спринт

--- app/src/main/java/com/practicum/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MediaLibraryActivity.kt
@@ -2,11 +2,13 @@ package com.practicum.playlistmaker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.practicum.playlistmaker.R.layout
 
 class MediaLibraryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_library)
+        setContentView(layout.activity_media_library)
         // будущий телепорт в "Медиа"
     }
 }
