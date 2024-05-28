e0cb283fe6887c2c882e80dcdf41ca809ef012a3
melval-coder <val2567@yandex.ru>
2024-05-28 21:24:44 +0500

    13 спринт

--- app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -8,25 +10,30 @@ import java.lang.reflect.Type
 const val TRACKS_KEY = "track_key"
 const val MAXIMUM = 10
 
-class SearchHistory {
+class SearchHistory(context: Context) {
 
     // Метод для добавления трека в историю поиска
-    fun setTrack(track: Track, sharedPreferences: SharedPreferences) {
-        val tracks = read(sharedPreferences)
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+        SHARED_PREFERENCES,
+        MODE_PRIVATE
+    )
+
+    fun setTrack(track: Track) {
+        val tracks = read()
         if (!tracks.remove(track) && tracks.size >= MAXIMUM) tracks.removeAt(MAXIMUM - 1)
         tracks.add(0, track)
-        write(sharedPreferences, tracks)
+        write(tracks)
     }
 
     // Метод для чтения истории треков из SharedPreferences
-    fun read(sharedPreferences: SharedPreferences): MutableList<Track> {
+    fun read(): MutableList<Track> {
         val json = sharedPreferences.getString(TRACKS_KEY, null) ?: return mutableListOf()
         val listOfMyClassObject: Type = object : TypeToken<ArrayList<Track>>() {}.type
         return Gson().fromJson(json, listOfMyClassObject)
     }
 
     // Метод для записи обновленной истории треков в SharedPreferences
-    private fun write(sharedPreferences: SharedPreferences, tracks: MutableList<Track>) {
+    private fun write(tracks: MutableList<Track>) {
         val json = Gson().toJson(tracks)
         sharedPreferences.edit()
             .putString(TRACKS_KEY, json)
