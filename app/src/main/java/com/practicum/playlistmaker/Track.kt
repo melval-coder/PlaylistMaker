3dc77056622790a6af4daedbffadf0a1ff38c97b
melval-coder <val2567@yandex.ru>
2024-06-12 22:22:49 +0500

    14 спринт

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -8,5 +8,6 @@ data class Track(
     val collectionName: String,
     val releaseDate: String,
     val primaryGenreName: String,
-    val country: String
+    val country: String,
+    val previewUrl: String
 )
